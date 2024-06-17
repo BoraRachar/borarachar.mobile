f8f6be555c35f9335d5448e9f4280ec9dfa596a9
Alysson Chiarelly <alyssonclr@gmail.com>
2024-06-17 06:34:51 -0300

    feat: foi adicionado a segunda etapa do formulário de cadastro de usuário

--- src/app/create-user/index.tsx
+++ src/app/create-user/index.tsx
@@ -3,7 +3,7 @@ import { styles } from './styles'
 import { styles as globalStyles } from '../styles'
 import Header from '../../components/HeaderComponent'
 import ProgressBarComponent from '../../components/ProgressBarComponent'
-import NameInput from './nameInput'
+import EmailInput from './emailInput'
 
 export default function CreateUser() {
   return (
@@ -11,7 +11,7 @@ export default function CreateUser() {
       <Header title="Criar conta" />
       <ProgressBarComponent totalSteps={3} currentStep={1} />
       <View style={globalStyles.formContainer}>
-        <NameInput />
+        <EmailInput />
       </View>
     </View>
   )
