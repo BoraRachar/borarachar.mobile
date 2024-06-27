8ab57ce83faa3d0d559e6b642c554f2f8f806509
Junior Alves <jrnalves@gmail.com>
2024-06-27 11:10:56 -0300

    feat: Criar a pagina inicial do Redefinir a senha

---
+++ src/app/reset-password/index.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+
+import Header from '@/src/components/HeaderComponent'
+
+export default function ResetPassword() {
+  return (
+    <View>
+      <View>
+        <Header title="Redefinir senha" />
+      </View>
+    </View>
+  )
+}
