bece08454dd09d6edcc7214626c7b7f1fbe0fc28
Junior Alves <jrnalves@gmail.com>
2024-07-04 17:01:48 -0300

    feat: Criar a pagina de sucesso da redefinição de senha

---
+++ src/app/reset-password/Success/index.tsx
@@ -0,0 +1,34 @@
+import { Pressable, Text, View } from 'react-native'
+import { styles } from './styles'
+import { useRouter } from 'expo-router'
+
+import CloseFramer from '@/src/assets/images/closeFrame.svg'
+import ResetPasswordSucess from '@/src/assets/images/reset-password-sucess.svg'
+
+export default function Sucess() {
+  const router = useRouter()
+
+  return (
+    <View style={{ flex: 1, paddingHorizontal: 24 }}>
+      <View style={{ paddingVertical: 25, alignSelf: 'flex-end' }}>
+        <Pressable onPress={() => router.replace('/')}>
+          {<CloseFramer />}
+        </Pressable>
+      </View>
+
+      <View style={{ paddingTop: 60, alignItems: 'center' }}>
+        <View style={{ marginTop: 24 }}>
+          <ResetPasswordSucess />
+        </View>
+
+        <View style={{ marginTop: 42 }}>
+          <Text style={styles.subtitle}>Senha redefinida com sucesso!</Text>
+          <Text style={styles.description}>
+            Enviaremos uma notificação por e-mail confirmando a criação de uma
+            nova senha.
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
