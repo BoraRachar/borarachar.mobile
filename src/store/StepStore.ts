c2c91f03b753ff7eece3d1661744229a012114ef
Alysson Chiarelly <alyssonclr@gmail.com>
2024-07-11 17:58:35 -0300

    Revert "Feature/name suggestion"

--- src/store/StepStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-import { Step } from '../interfaces/types'
-
-export const useStepStore = create<Step>((set) => ({
-  step: 1,
-  increaseStep: () => set((state) => ({ step: state.step + 1 })),
-  decreaseStep: () => set((state) => ({ step: state.step - 1 })),
-}))
--- src/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import { colors } from './colors'
-import { fontFamily } from './font-family'
-import { sizes } from './sizes'
-
-export const theme = {
-  colors,
-  fontFamily,
-  sizes,
-}
