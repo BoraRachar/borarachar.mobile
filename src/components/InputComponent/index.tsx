45938f398c4994a57a36f709d7df7fe4fe025b9d
Alysson Chiarelly <alyssonclr@gmail.com>
2024-08-29 18:56:20 -0300

    refactory: a solução encontrada foi refatorada

--- src/components/InputComponent/index.tsx
+++ src/components/InputComponent/index.tsx
@@ -64,7 +64,7 @@ const InputComponent: React.FC<InputComponentProps> = ({
           cursorColor={theme.colors.Gray[500]}
           value={value}
           onChangeText={onChangeText}
-          maxLength={55}
+          style={styles.input}
         />
         {errorOrSucess ? (
           <WarningCircle />
