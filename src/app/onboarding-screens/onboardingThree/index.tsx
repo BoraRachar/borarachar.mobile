c75925231cdbfd00a52ebb53afda52f12a426734
Alysson Leite <alysson_leite@yahoo.com.br>
2024-09-02 11:34:00 -0300

    Fixing paddings

--- src/app/onboarding-screens/onboardingThree/index.tsx
+++ src/app/onboarding-screens/onboardingThree/index.tsx
@@ -44,7 +44,7 @@ export default function OnboardingScreenThree() {
             </Text>
           </View>
         </View>
-        <View>
+        <View style={styles.linkContainer}>
           <ButtonCustomizer.Root
             type={'primary'}
             onPress={() => router.push('/')}
