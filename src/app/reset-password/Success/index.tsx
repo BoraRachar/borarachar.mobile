39f7b1381bd62fb920c45da6796c9ced4c09fa33
Junior Alves <jrnalves@gmail.com>
2024-09-06 10:27:22 -0300

    fix:standardize login screen
     - Adicionar botão de "fazer login" na tela de secesso quando trocar a senha.
     - Ajustar altura e espaçamento da tela

--- src/app/reset-password/Success/index.tsx
+++ src/app/reset-password/Success/index.tsx
@@ -3,22 +3,24 @@ import { useRouter } from 'expo-router'
 
 import CloseFramer from '@/src/assets/images/closeFrame.svg'
 import ResetPasswordSucess from '@/src/assets/images/reset-password-sucess.svg'
+import { ButtonCustomizer } from '@/src/components/ButtonCustomizer'
 
+import { styles as globalStyles } from '@/src/app/styles'
 import { styles } from './styles'
 
 export default function Sucess() {
   const router = useRouter()
 
   return (
-    <View style={{ flex: 1, paddingHorizontal: 24 }}>
+    <View style={{ flex: 1, justifyContent: 'space-between', padding: 24 }}>
       <View style={{ paddingVertical: 25, alignSelf: 'flex-end' }}>
         <Pressable onPress={() => router.replace('/')}>
           {<CloseFramer />}
         </Pressable>
       </View>
 
-      <View style={{ paddingTop: 60, alignItems: 'center' }}>
-        <View style={{ marginTop: 24 }}>
+      <View style={{ alignItems: 'center' }}>
+        <View>
           <ResetPasswordSucess />
         </View>
 
@@ -29,6 +31,19 @@ export default function Sucess() {
             nova senha.
           </Text>
         </View>
+      </View>
+
+      <View>
+        <ButtonCustomizer.Root
+          type="primary"
+          customStyles={globalStyles.primaryButton}
+          onPress={() => router.push('/login')}
+        >
+          <ButtonCustomizer.Title
+            title="Fazer Login"
+            customStyles={globalStyles.primaryButtonText}
+          />
+        </ButtonCustomizer.Root>
       </View>
     </View>
   )
