89b2ff1569dfe6647fbb5a162cf77032b6a0c74f
Junior Alves <jrnalves@gmail.com>
2024-09-24 17:52:30 -0300

    [BR-107]Feat: Criar o header e os botões
     - Criar os headers com todas as personalizações do titulo e icones
     - Criar o espaço que receberá o componentes de slide
     - Criar os botões

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -1,16 +1,67 @@
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { Text, View } from 'react-native'
 
+import { ButtonCustomizer } from '@/src/components/ButtonCustomizer'
+
+import { theme } from '@/src/theme'
+import { styles as globalStyles } from '@/src/app/styles'
+import { styles } from './styles'
+
 export default function Home() {
   return (
-    <View>
-      <View>
-        <Text>Ola, Bora</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Ola, Bora.</Text>
 
-        <View>
-          <Ionicons name="notifications-outline" size={24} color="black" />
-          <Ionicons name="help-circle-outline" size={24} color="black" />
+        <View style={styles.headerIcon}>
+          <Ionicons
+            name="notifications-outline"
+            size={24}
+            color={theme.colors.primaryColor}
+          />
+          <Ionicons
+            name="help-circle-outline"
+            size={24}
+            color={theme.colors.primaryColor}
+          />
         </View>
+      </View>
+
+      <View style={{ gap: 12, paddingVertical: 10 }}>
+        <View style={{ height: 24, backgroundColor: 'gray' }}></View>
+        <View style={{ height: 130, backgroundColor: 'gray' }}></View>
+        <View style={{ height: 8, backgroundColor: 'gray' }}></View>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <ButtonCustomizer.Root
+          type="tertiaryHalfWidth"
+          onPress={() => console.log('Meu QR')}
+        >
+          <ButtonCustomizer.Title
+            title="Meu QR"
+            customStyles={globalStyles.tertiaryButtonText}
+          />
+        </ButtonCustomizer.Root>
+
+        <ButtonCustomizer.Root
+          type="primaryHalfWidth"
+          onPress={() => console.log('Calculadora')}
+          customStyles={globalStyles.primaryButtonHalfWidth}
+        >
+          <ButtonCustomizer.Title
+            title="Calculadora"
+            customStyles={globalStyles.primaryButtonText}
+          />
+          {/* <ButtonCustomizer.Icon
+            icon={isButtonDisable ? ArrowRightDisable : ArrowRight}
+            customStyles={
+              isButtonDisable
+                ? globalStyles.primaryButtonIconDisabled
+                : globalStyles.primaryButtonIcon
+            }
+          /> */}
+        </ButtonCustomizer.Root>
       </View>
     </View>
   )
---
+++ src/app/(tabs)/home/styles.ts
@@ -0,0 +1,32 @@
+import { theme } from '@/src/theme'
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: theme.colors.white,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  headerIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  title: {
+    fontFamily: theme.fontFamily.bold,
+    fontSize: theme.sizes.fontSize24,
+    lineHeight: 30,
+    color: theme.colors.primaryColor,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 24,
+  },
+})
