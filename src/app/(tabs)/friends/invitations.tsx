47a4a6ca3a1dced782a4377dd56cee9ff294b2b4
Junior Alves <jrnalves@gmail.com>
2024-10-24 17:41:57 -0300

    feat: criar o componente de botão "adicionar amigos"
    - Criar o componente de botão "adicionar amigo"
    - Adicionar o componente nas paginas Amigos e Convite

--- src/app/(tabs)/friends/invitations.tsx
+++ src/app/(tabs)/friends/invitations.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import AddFriendButton from '@/src/components/AddFriendButton'
 
 import { styles } from './styles'
 
 export default function Invitations() {
   return (
     <View style={styles.container}>
-      <Text>Invitations</Text>
+      <AddFriendButton />
     </View>
   )
 }
