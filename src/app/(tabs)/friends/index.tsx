623bdc15b8dae717a4c8f524eef0f1fe14030906
Junior Alves <jrnalves@gmail.com>
2024-10-24 11:45:16 -0300

    feat: Criar o texto e o botão de adicionar amigo
    - Criar o text "Adicionar novo amigo" com os estilos do styleSheet
    - Criar o botão addicionar Amigo com icone do Plus
    - Realizar as formatações no stylesheet deste componente
    - Corrigir bugs de escrita e refactory de nome de classe

--- src/app/(tabs)/friends/index.tsx
+++ src/app/(tabs)/friends/index.tsx
@@ -1,13 +1,15 @@
 import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { styles } from './styles'
+
+import Plus from '@/src/assets/images/plus.svg'
 
 export default function Amigos() {
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.text}>Em Cibely, você tem:</Text>
+      <View style={styles.resumeContent}>
+        <Text style={styles.text}>Cibely, você tem:</Text>
         <Text style={styles.text}>10 amigos</Text>
         <View style={styles.containerText}>
           <Text style={styles.text}>4 convites enviados</Text>
@@ -27,6 +29,17 @@ export default function Amigos() {
             Ver
           </Link>
         </View>
+      </View>
+
+      <View style={styles.containerButton}>
+        <Text style={[styles.text, styles.textButton]}>
+          Adicionar novo amigo
+        </Text>
+        <TouchableOpacity>
+          <View style={styles.addButton}>
+            <Plus />
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   )
