1d7d91c46c420dbe594c767c4e8d8f69c4ae4bda
Junior Alves <jrnalves@gmail.com>
2024-10-29 11:10:25 -0300

    [BR-145] feature(create friends list): Criar a lista de amigos na pagina lista de amigos (#66)
    * feat: Criar a seção e o componente lista de amigos na pagina Amigos
    
     - Criar a seção com o titulo Amigos
     - Cria o componentes com o avatar vazio, nome do amigo, grupos em comum
       e seta para o link do perfil
    
    * feat: Ajustar a lista de amigos para flatList
    
     - Colocar a lista de amigos dentro da faltlist
     - Criar o mock com a lista de amigos que furutamente será substituido
       pelos dados da API
     - Separar o componente de amigo para FriendItem que recebe o objeto com nome, imagem e
       grupos
     - Gera a lista de amigos de forma dinamica recebendo o amigo por paramentro
     - Configurar a tipagem
     - Ajustar os stilus
    
    * fix: Corrigir o tamanho da foto do avatar
    
    - Corrigir o tamanho para ajustar em todas a foto em todas as telas
    
    * feat: Adicionar o usuario que vem na API na primeira frase da pagina

--- src/app/(tabs)/friends/styles.ts
+++ src/app/(tabs)/friends/styles.ts
@@ -13,11 +13,24 @@ export const styles = StyleSheet.create({
     paddingHorizontal: horizontalScale(24),
     paddingVertical: verticalScale(24),
   },
+  resumeContent: {
+    gap: verticalScale(20),
+  },
+  title: {
+    fontFamily: theme.fontFamily.bold,
+    fontSize: rem(24),
+    lineHeight: verticalScale(30),
+    color: theme.colors.primaryColor,
+    marginTop: verticalScale(16),
+  },
   text: {
     fontSize: rem(16),
     lineHeight: verticalScale(24),
     fontFamily: theme.fontFamily.regular,
     color: theme.colors.primaryColor,
+  },
+  textBold: {
+    fontFamily: theme.fontFamily.bold,
   },
   textButton: {
     fontFamily: theme.fontFamily.semiBold,
@@ -25,5 +38,39 @@ export const styles = StyleSheet.create({
   containerText: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  containerButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: verticalScale(12),
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.Gray[300],
+    marginTop: verticalScale(22),
+  },
+  addButton: {
+    backgroundColor: theme.colors.third,
+    borderRadius: 50,
+    padding: 14,
+  },
+  contentFriend: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: horizontalScale(16),
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.third,
+    paddingVertical: verticalScale(12),
+  },
+  avatarContainer: {
+    backgroundColor: theme.colors.third,
+    borderRadius: 50,
+    padding: 14,
+  },
+  avatarImage: {
+    flex: 1,
+    width: 48,
+    height: 48,
+    borderRadius: 50,
+    resizeMode: 'cover',
   },
 })
---
+++ src/mock/friends.ts
@@ -0,0 +1,62 @@
+export const friends = [
+  {
+    id: 1,
+    name: 'Junior Alves',
+    avatar: 'https://github.com/junioralvesbr.png',
+    groups: 2,
+  },
+  {
+    id: 2,
+    name: 'Alysson Chiarelly',
+    avatar: 'https://github.com/devalyssonchiarelly.png',
+    groups: 1,
+  },
+  {
+    id: 3,
+    name: 'Alysson Chiarelly',
+    avatar: 'https://github.com/alysson-leite.png',
+    groups: 5,
+  },
+  {
+    id: 4,
+    name: 'Matheus Meneses',
+    avatar: 'https://github.com/MatGAAM.png',
+    groups: 5,
+  },
+  {
+    id: 5,
+    name: 'Sandra BUllock',
+    avatar: '',
+    groups: 6,
+  },
+  {
+    id: 6,
+    name: 'Junior Alves',
+    avatar: 'https://github.com/junioralvesbr.png',
+    groups: 2,
+  },
+  {
+    id: 7,
+    name: 'Alysson Chiarelly',
+    avatar: 'https://github.com/devalyssonchiarelly.png',
+    groups: 1,
+  },
+  {
+    id: 8,
+    name: 'Alysson Chiarelly',
+    avatar: 'https://github.com/alysson-leite.png',
+    groups: 5,
+  },
+  {
+    id: 9,
+    name: 'Matheus Meneses',
+    avatar: 'https://github.com/MatGAAM.png',
+    groups: 5,
+  },
+  {
+    id: 10,
+    name: 'Sandra BUllock',
+    avatar: '',
+    groups: 6,
+  },
+]
