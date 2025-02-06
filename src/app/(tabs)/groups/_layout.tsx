2541329443e4b4689abbed1354ad8e4e05ec1dde
Junior Alves <jrnalves@gmail.com>
2025-02-06 17:07:35 -0300

    feat: Criar o arquivo _layout.tsx na raiz da pasta groups
     - Configurar no arquivo _layout.tsx o expo router para navegação em STACK
     - Configura o barra de navegação utilizando a opções do expo router
       stack.screen

---
+++ src/app/(tabs)/groups/_layout.tsx
@@ -0,0 +1,27 @@
+import { router, Stack } from 'expo-router'
+
+import LeftIcon from '@/src/assets/images/arrowBack.svg'
+import Question from '@/src/assets/images/question.svg'
+
+export default function GroupsLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Meus grupos',
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerLeft: () => (
+            <LeftIcon
+              onPress={() => {
+                router.back()
+              }}
+            />
+          ),
+          headerRight: () => <Question />,
+        }}
+      />
+    </Stack>
+  )
+}
