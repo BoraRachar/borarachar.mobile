1749afd58acf876eabd98bf648be5777762b7528
João Filho <joaofilholimati@gmail.com>
2025-08-24 20:35:05 -0300

    fix: ajuste da estilização do botão (aplicar), para que fique de acordo com a historia e desabilite o botão quando não estiver nenhuma data selecionada

--- src/components/CalendarComponent/FooterButtons.tsx
+++ src/components/CalendarComponent/FooterButtons.tsx
@@ -21,9 +21,15 @@ export function FooterButtons({ onClear, onApply, canApply }: {
                 />
             </ButtonCustomizer.Root>
 
-            <ButtonCustomizer.Root type="primaryHalfWidth" onPress={onApply} disabled={!canApply} customStyles={{
-                borderWidth: 0,
-            }}>
+            <ButtonCustomizer.Root
+                type="primaryHalfWidth"
+                onPress={onApply}
+                disabled={!canApply}
+                customStyles={{
+                    borderWidth: 0,
+                    backgroundColor: canApply ? theme.colors.primaryColor : theme.colors.fourth,
+                }}
+            >
                 <ButtonCustomizer.Title
                     title="Aplicar"
                     customStyles={calendarStyles.primaryButtonText}
