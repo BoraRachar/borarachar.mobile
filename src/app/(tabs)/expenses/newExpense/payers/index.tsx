110f52faa946d8f705ea4084c37a61d81cefd59e
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-09-02 16:19:40 -0300

    feat: finalizada página 1.3.3

--- src/app/(tabs)/expenses/newExpense/payers/index.tsx
+++ src/app/(tabs)/expenses/newExpense/payers/index.tsx
@@ -1,9 +1,144 @@
-import { Text, View } from "react-native";
+import {
+  FlatList,
+  KeyboardAvoidingView,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { useState, useEffect } from "react";
+import { axiosPrivateClient } from "@/src/utils/axios";
+import { useExpenseStore } from "@/src/store/useExpenseStore";
+import { useAuthStore } from "@/src/store/useAuthStore";
+
+import ProgressBarComponent from "@/src/components/ProgressBarComponent";
+import Checkbox from "expo-checkbox";
+import { ButtonCustomizer } from "@/src/components/ButtonCustomizer";
+import UserIcon from "@/src/assets/images/user.svg";
+import { Ionicons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+import { theme } from "@/src/theme";
+
+interface ParticipantType {
+  participanteId: string;
+  nome: string;
+  email: string;
+  apelido: string;
+  isAdm: boolean;
+  hasPendent: boolean;
+}
 
 export default function Payers() {
-    return (
-        <View>
-            <Text>Payers</Text>
-        </View>
-    );
+  const { expenseData, setExpenseData } = useExpenseStore();
+  const { userCod } = useAuthStore();
+
+  const [participants, setParticipants] = useState<ParticipantType[]>([]);
+  const [selectedParticipants, setSelectedParticipants] = useState<string[]>(
+    []
+  );
+
+  
+  useEffect(() => {
+      const fetchParticipants = async () => {
+          try {
+              const { data } = await axiosPrivateClient.get(
+                  "participantes/lista-participantes",
+                  {
+                      params: {
+                          grupoId: expenseData.grupoId,
+                        },
+                    }
+                );
+                
+                setParticipants(data.data);
+                console.log(participants);
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+        };
+        fetchParticipants();
+    }, [userCod]);
+
+    const toggleSelection = (participantId: string) => {
+      setSelectedParticipants((prev) =>
+        prev.includes(participantId)
+          ? prev.filter((item) => item !== participantId)
+          : [...prev, participantId]
+      );
+      console.log(selectedParticipants);
+    };
+
+    function handleNext(){
+        if(selectedParticipants.length === 0){
+            return
+        }
+        setExpenseData({
+            ...expenseData,
+             pagadores: selectedParticipants
+            })
+        console.log("Expense Data Updated: ", {...expenseData, pagadores: selectedParticipants})
+    }
+    
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <ProgressBarComponent totalSteps={100} currentStep={80} />
+
+      <View style={styles.containerDescription}>
+        <Text style={styles.title}>
+          Selecione a pessoa que irá pagar pela despesa.
+        </Text>
+        <Text style={styles.text}>
+          Selecione quem irá <Text style={styles.textBold}>dividir</Text> o
+          valor desta despesa
+        </Text>
+      </View>
+
+      <View style={styles.containerList}>
+        <FlatList
+          data={participants ?? []}
+          keyExtractor={(item) => item.participanteId}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.containerItem}>
+              <View style={styles.itemContent}>
+                <View style={styles.containerUser}>
+                  <View style={styles.userIcon}>
+                    <UserIcon width={26} height={26} />
+                  </View>
+
+                  <Text style={styles.checkboxText}>{item.nome}</Text>
+                </View>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={selectedParticipants.includes(item.participanteId)}
+                  onValueChange={() => toggleSelection(item.participanteId)}
+                  color={
+                    selectedParticipants.includes(item.participanteId)
+                      ? theme.colors.primaryColor
+                      : undefined
+                  }
+                />
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+
+                <View style={styles.containerButton}>
+                  <ButtonCustomizer.Root
+                    type="primary"
+                    onPress={handleNext}
+                    disabled={selectedParticipants.length === 0}
+                    customStyles={selectedParticipants.length === 0 ? styles.disabledButton : styles.nextButton}
+                  >
+                    <ButtonCustomizer.Title
+                      title="Credor"
+                      customStyles={styles.nextButtonText}
+                    />
+                    <Ionicons name="arrow-forward" size={20} color={theme.colors.white} style={{ marginLeft: 8 }} />
+                  </ButtonCustomizer.Root>
+                </View>
+
+    </KeyboardAvoidingView>
+  );
 }
---
+++ src/app/(tabs)/expenses/newExpense/payers/styles.ts
@@ -0,0 +1,107 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '@/src/theme'
+import { verticalScale, horizontalScale, rem } from '@/src/utils/responsiveUtils'
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.white,
+        paddingHorizontal: horizontalScale(24),
+        paddingVertical: verticalScale(24),
+    },
+    containerDescription:{
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    title:{
+        fontFamily: theme.fontFamily.bold,
+        fontSize: rem(24),
+        lineHeight: 24,
+        color: theme.colors.primaryColor,
+        paddingVertical: verticalScale(12),
+    },
+    text:{
+        fontFamily: theme.fontFamily.regular,
+        fontSize: 18,
+        lineHeight: 24,
+        color: theme.colors.primaryColor
+    },
+    textBold:{
+        fontFamily: theme.fontFamily.bold,
+        fontSize: 18,
+        lineHeight: 24,
+        color: theme.colors.primaryColor
+    },
+    containerList:{
+        flex: 1,
+        paddingVertical: verticalScale(12),
+    },
+    containerItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 8,
+        paddingVertical: verticalScale(10),
+        borderBottomWidth: 1,
+        borderBottomColor: theme.colors.Gray[300],
+    },
+    itemContent:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 16
+    },
+    containerUser:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8
+    },
+    userIcon:{
+        backgroundColor: theme.colors.third,
+        borderRadius: 50,
+        padding: 8,
+        width: 48,
+        height: 48,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    checkbox: {
+        borderColor: theme.colors.primaryColor,
+        borderWidth: 2,
+        borderRadius: 4,
+    },
+    checkboxText: {
+        fontFamily: theme.fontFamily.regular,
+        fontSize: 16,
+        color: theme.colors.primaryColor,
+    },
+    containerButton:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: verticalScale(10),
+        borderBottomWidth: 1,
+        borderBottomColor: theme.colors.Gray[300],
+    },
+    nextButton: {
+        flex: 1,
+        height: 48,
+        backgroundColor: theme.colors.Gray[600],
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 8,
+    },
+    nextButtonText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: theme.colors.white,
+        fontFamily: theme.fontFamily.semiBold,
+    },
+    disabledButton: {
+        backgroundColor: theme.colors.fourth,
+    }
+})
