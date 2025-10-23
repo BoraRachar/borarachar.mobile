71b3279c4c360ac29d15bfb3ecd9387f57f0b1f4
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-10-23 09:44:20 -0300

    feat: criar estrutura base da tela 2.0

--- src/app/(tabs)/activity/index.tsx
+++ src/app/(tabs)/activity/index.tsx
@@ -1,11 +1,14 @@
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Link } from "expo-router";
+import { Text, View, KeyboardAvoidingView } from "react-native";
+import InputComponent from "@/src/components/InputComponent";
 
 export default function Activity() {
   return (
-    <View>
-      <Text>Activity</Text>
-      <Link href="/(tabs)/activity/userActivity">userActivity</Link>
-    </View>
-  )
+    <KeyboardAvoidingView>
+      <View>
+        <Text>Minhas atividades</Text>
+        <InputComponent placeholder="Nome, grupo, data..." />
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
